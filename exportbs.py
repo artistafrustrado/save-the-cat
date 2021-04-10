57f46a37730b59cedfaabf892acae351bbc0de99
Fernando Michelotti <artista.frustrado@gmail.com>
2021-04-10 03:36:31 -0300

    added exports file

---
+++ exportbs.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+class BeatSheetExporter:
+    def __init__(self, dataset):
+        pass
+
+    def save(self):
+        pass
+
+class BeatSheetExporterLatex:
+    def __init__(self):
+        pass
+
+
+class BeatSheetExporterHtml:
+    def __init__(self):
+        pass
+
+
+class BeatSheetExporterRtf:
+    def __init__(self):
+        pass
+
+
+class BeatSheetExporterOdt:
+    def __init__(self):
+        pass
+
+#from odf.opendocument import OpenDocumentText
+#from odf.text import P    
+#textdoc = OpenDocumentText()
+#p = P(text="Hello World!")
+#textdoc.text.addElement(p)
+#textdoc.save("helloworld", True)
+#
+# https://gist.github.com/balasankarc/1832670ec8ddd9a34d33
+# https://balasankarc.in/tech/using-python-and-odfpy-to-create-open-document-texts.html
